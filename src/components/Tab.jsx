1ef54b33be08f1ddd4389e0c3cdaa74ba92ff7b1
Starlight <linjerome95@gmail.com>
2025-07-06 21:56:41 +0200

    Tab style

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import About from "../pages/About.jsx";
+import "../styles/Tab.css";
 
 export function TabsRoutes() {
     return (
