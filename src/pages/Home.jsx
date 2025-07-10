504fb163b79ddef5de175aad5fe3f1e9b83b970e
Starlight <linjerome95@gmail.com>
2025-07-10 20:36:14 +0200

    add battlesuits images homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import Tabs from "../components/Tab.jsx";
 import "../styles/Home.css";
 
-const title =
-    <div className="home-title">
+function Title() {
+    return (<div className="home-title">
         <h1>Elysian Realm Guide</h1>
         <h2>Recommended build v8.3</h2>
-    </div>;
+    </div>);
+}
+
+function getBattlesuitsImagesPaths() {
+    const files = import.meta.glob("../data/Builds/*", {eager: true});
+    return Object.keys(files).map(
+        path => {
+            const fullname = path.split("/").pop();
+            const battlesuitImage = "images/battlesuits/" + fullname.replace(".json", ".webp");
+            return battlesuitImage;
+        }
+    );
+}
+
+function RenderBattlesuitsImages() {
+    return getBattlesuitsImagesPaths().map(battlesuitsImage => <img src={battlesuitsImage}/>);
+}
 
 function Home() {
-    return (
-        <div className="home-container">
-            {title}
-            <Tabs/>
-        </div>
-    );
+    return (<div className="home-container">
+        <Title/>
+        <Tabs/>
+        <RenderBattlesuitsImages/>
+    </div>);
 }
 
 export default Home;
