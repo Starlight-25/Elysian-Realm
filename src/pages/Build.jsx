7ea513ed35d49212e5a385946fee007e78cfd0c9
Starlight <linjerome95@gmail.com>
2025-07-10 21:25:33 +0200

    Title in build of battlesuits

--- src/pages/Build.jsx
+++ src/pages/Build.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
+import Tabs from "../components/Tab.jsx";
+
+function Title({name}) {
+    return (
+        <div className="build-title">
+            <h1>{name.replace(/_/g, " ")}</h1>
+            <img src={`/Elysian-Realm/images/battlesuits/${name}.webp`}/>
+        </div>
+    );
+}
 
 function BuildPage() {
-    const { name } = useParams();  // récupère le battlesuit sélectionné
+    const {name} = useParams();  // récupère le battlesuit sélectionné
     return (
         <div>
-            <h1>Build: {name.replace(/_/g, " ")}</h1>
+            <Title name={name}/>
+            <Tabs/>
         </div>
     );
 }
